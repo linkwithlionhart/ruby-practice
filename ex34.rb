5597f5af3a34722afbc0bfaadb168b836410cd32
linkwithlionhart <linkwithlionhart@gmail.com>
2023-11-27 21:20:20 -0800

    complete ex34

---
+++ ex34.rb
@@ -0,0 +1,38 @@
+# Ordinal indicates position, index at zero.
+# Cardinal indicates count.
+# Remember: ordinal == ordered, 1st; cardinal == cards at random, 0.
+
+# Let's practice this. 
+# Take this array of animals, and follow the exercises where I tell you to write down what animal you get for that ordinal or cardinal number. 
+# Remember, if I say "first," "second," then I'm using ordinal, so subtract 1. 
+# If I give you cardinal (like "The animal at 1"), then use it directly.
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# The animal at 1.
+puts animals[1]
+
+# The third (3rd) animal.
+puts animals [2-1]
+puts animals[2]
+
+# The first (1st) animal.
+puts animals [1-1]
+puts animals[0]
+
+# The animal at 3.
+puts animals[3]
+
+# The fifth (5th) animal.
+puts animals[5-1]
+puts animals[4]
+
+# The animal at 2.
+puts animals[2]
+
+# The sixth (6th) animal.
+puts animals[6-1]
+puts animals[5]
+
+# The animal at 4.
+puts animals[4]
